0bd4b4f8e23e86abab975efc126cfad94b4f3edd
orasa oneal <orasaoneal@gmail.com>
2019-06-20 01:25:23 -0700

    js

---
+++ client/js/scripts.js
@@ -0,0 +1,12 @@
+console.log("Hello from JS");
+document.console.log(document);
+
+// get id from html, we pass id name into
+let wrap = document.getElementById('wrap')
+
+console.log(wrap)
+
+// window
+
+window.onload = () => { console.log('window is loaded')
+}
