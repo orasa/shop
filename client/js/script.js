68508bab26e00701fa09110cc276cd82b6f7536a
Tony Russo <tony@tortugacoders.com>
2019-06-21 09:40:08 +0700

    convert products into objects

--- client/js/script.js
+++ client/js/script.js
@@ -21,7 +21,21 @@ window.onload = () => {
 	// target products
 	let products_ui = document.getElementById('products')
 	// define products
-	let products = ['1', '2', '3']
+	let products = [{
+		title: '1',
+		price: 200,
+		description: 'Air Max'
+	},
+	{
+		title: '2',
+		price: 150,
+		description: 'Adidas Yo'
+	},
+	{
+		title: '3',
+		price: 690,
+		description: 'Hahaha'
+	}]
 	// display each product in the DOM
 	products.forEach((p) => {
 		products_ui.insertAdjacentHTML('beforeEnd', `
@@ -31,11 +45,11 @@ window.onload = () => {
 			</div>
 			<div class="product-extras">
 				<div class="description">
-					<h4>${p}</h4>
-					<small>Air Max 270</small>
+					<h4>${p.title}</h4>
+					<small>${p.description}</small>
 				</div>
 				<div class="price">
-					<span>$189.99</span>
+					<span>$${p.price}</span>
 					<a href="#" class="button">shop now</a>
 				</div>
 			</div>
